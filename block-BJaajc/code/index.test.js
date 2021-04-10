37b12faeab111917d094634c33c8d686a860379b
siddharth sharma <sharma.siddharth1610@gmail.com>
2021-04-10 11:30:36 +0530

    Test with jest

--- block-BJaajc/code/index.test.js
+++ block-BJaajc/code/index.test.js
@@ -1,6 +1,6 @@
 const { TestScheduler } = require("@jest/core");
 const clone = require("./index");
 
-TestScheduler('clone of object {name: "sam"} equal {name: "sam"}', () => {
+test('clone of object {name: "sam"} equal {name: "sam"}', () => {
     expect(cloneObject({name: "sam"})).toBe({name: "sam"})
 });
